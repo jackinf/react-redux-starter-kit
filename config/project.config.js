e44f9a27666a865084e2fe517e8df7808272eba2
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-05-01 11:41:01 +0300

    experimenting with strong types

--- config/project.config.js
+++ config/project.config.js
@@ -24,7 +24,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_host : "localhost", //TODO: use ip.address(), but i'm using localhost hack for Hot reload to work, // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
